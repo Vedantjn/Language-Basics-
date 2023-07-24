cb977ee658b809545a126d94fb049522bf56bb0d
Vedant Jain <vedant.jain.indore@gmail.com>
2023-07-24 18:44:57 +0530

    JS and Python basics

---
+++ JS-Basics/index.js
@@ -0,0 +1,43 @@
+// console.log('Hello world');
+
+// let name = 'Vedant';
+// console.log(name);
+
+// const name = 7;
+// name = 2;
+// console.log(name)
+
+// let name = 'Vedant'; // String literal
+// let age = 30; // Number literal
+// let isApproved = true; // Boolean literal
+// let firstName = undefined;
+// let lastName = null;
+
+// let person = {
+//     name : 'Vedant', 
+//     afe : 21
+// };
+
+// person['name'] = 'sharma';
+// person.name = 'jain'
+
+// console.log(person);
+
+// let selectedColours = ['red', 'blue'];
+// selectedColours[2] = 'green';
+// selectedColours[3] = '1';
+// console.log(selectedColours);
+// console.log(selectedColours.length);
+// console.log(selectedColours[0]);
+
+// function greet(name){ // name -> parameter
+//     console.log('Hello ' + name);
+// }
+
+// greet('Vedant'); // Vedant -> argument
+
+function square(num){
+    return num*num;
+}
+
+console.log(square(2));
---
+++ Python-Basics/app.py
@@ -0,0 +1,148 @@
+# print("Hello world")
+
+# age = 20
+# price = 19.95
+# first_name = "Vedant"
+# is_online = True
+# print(age)
+
+# name = 'Vedant'
+# age = 21
+# type = "new patient"
+# print(name, age, type)
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year : ")
+# age = 2023 - int(birth_year)
+# print(age)
+
+# int()
+# float()
+# bool()
+# str()
+
+# num1 = float(input("Enter first number "))
+
+# num1 = input("Enter first number ")
+# num2 = input("Enter second number ")
+# sum = int(num1) + int(num2)
+# # print(sum)
+# print("Sum is : " + str(sum))
+
+# course = 'Python for Beginners'
+# print(course.upper())
+# print(course.lower())
+# print(course.find('y'))
+# print('Python' in course)
+# print(course.find('for'))
+# print(course.replace('for', '7'))
+# print(course)
+
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print(10 ** 3)
+
+# x = 10
+# x += 3
+# print(x)
+
+# x = 10 + 3 * 2
+# print(x)
+# x = (10 + 3) * 2
+# print(x)
+
+# x = 3 > 2
+# print(x)
+# x = 3 == 2
+# print(x)
+# x = 3 != 2
+# print(x)
+
+# price = 25
+# print(price > 10 and price < 34)
+# print(price > 10 or price < 34)
+# price = 5
+# print(not price > 10)
+
+# temperature = 25
+# if temperature > 30 :
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif temperature > 20 :
+#     print("It's a nice day")
+# elif temperature > 10 :
+#     print("It's a bit cold day")
+# else :
+#     print("It's cold")
+# print("Done")
+
+# weight = float(input("Weight"))
+# unit = input("(K)g or (L)bs")
+# if unit.upper() == "K":
+#     converted = weight / 0.45
+#     print("Weight in pounds is : " + str(converted))
+# else :
+#     converted = weight * 0.45
+#     print("Weight in kg is : " + str(converted))
+
+# i = 1
+# while i <= 10:
+#     print(i * '*')
+#     i = i + 1
+
+# names = ["John", "Bob", "Mosh", "Ved"]
+# print(names)
+# print(names[2])
+# print(names[-1])
+# print(names[3])
+# names[0] = "Jon"
+# print(names)
+# print(names[0:3])
+# print(names)
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.append(6)
+# numbers.insert(6, 7)
+# print(numbers) 
+# # numbers.clear()/
+# print(numbers) 
+# print(1 in numbers)
+# print(len(numbers))
+
+
+# numbers = [1, 2, 3, 4, 5]
+# print(numbers)
+
+# for i in numbers:
+#     print(i)
+
+
+# j = 0
+# while j < len(numbers):
+#     print(numbers[j])
+#     j = j + 1
+
+
+# numbers = range(5)
+# print(numbers)
+
+# for i in numbers:
+#     print(i)
+
+
+# numbers = range(5, 10)
+# print(numbers)
+
+# for i in numbers:
+#     print(i)
+
+
+# for i in range(5):
+#     print(i)
+
+
+numbers = (1, 2, 3, 3)
+numbers.count(3)
